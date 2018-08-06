7f6eeec5f6ca3578912f2e1552aa77470800982e
Daniel Rejniak <drejniak@ie.ibm.com>
2018-08-06 20:10:15 +0100

    Added new logo and site title

--- node-backend/src/controllers/private_controller.js
+++ node-backend/src/controllers/private_controller.js
@@ -1,5 +1,10 @@
 const bodyParser = require("body-parser");
+const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 
+var visualRecognition = new VisualRecognitionV3({
+  version: '2018-03-19',
+  iam_apikey: 'QtO_Sscty7KuQ7ObjnqlSYsU0-2hvFf0Ybvm43hWoCA1'
+});
 
 exports.check = (req, res, next) => {
     res.status(201).json({
